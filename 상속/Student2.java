4bff693a784548790065eefcb719eadc494c341d
olsi10 <wangbampang2035@gmail.com>
2022-04-28 00:04:02 +0900

    "홍길동은 학급회장이다." 출력 결과 도출

--- 상속/Student2.java
+++ 상속/Student2.java
@@ -19,10 +19,29 @@ public class Student2 {
     public void extra_act() {System.out.println("동아리 활동을 하다.");}
 }
 
+class Leader2 extends Student2 {
+
+    Boolean leader;
+
+    public Leader2(String name, String hakbun, Boolean leader) {
+        this.name = name;
+        this.hakbun = hakbun;
+        this.leader = leader;
+    }
+
+    void isLeader() {
+        if(leader == true) {
+            System.out.println(this.name + "은 학급회장이다.");
+        } else {
+            System.out.println(this.name + "은 학급회장이 아니다.");
+        }
+    }
+}
+
 class Mainclass {
     public static void main(String[] args) {
-        //Leader hong = new Leader("홍길동", "30130", true);
-                //hong.isLeader();
+        Leader2 hong = new Leader2("홍길동", "30130", true);
+                hong.isLeader();
     }
 }
 
