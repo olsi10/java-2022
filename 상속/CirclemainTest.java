e332f53470680aff858760ab85314242fb0f40e3
olsi10 <wangbampang2035@gmail.com>
2022-04-28 00:18:11 +0900

    priavate는 상속관계에서 하위 클래스 접근 불가하다.

--- 상속/CirclemainTest.java
+++ 상속/CirclemainTest.java
@@ -1,15 +1,17 @@
 package 상속;
 //에러 발생하는 이유 찾기
-class CirclArea_Exam {
-    private double area(int r) {
+class CircleArea_Exam {
+    //private double area(int r) { priavte는 상속관계에서 하위 크래스 접근 불가하다.
+    protected double area(int r) {
         return 3.14 * r * r;
     }
+
     protected double round(int r){
         return 2 * 3.14 * r;
     }
 }
 
-class CirclemainTest extends CirclArea_Exam {
+class CirclemainTest extends CircleArea_Exam {
     public static void main(String[] args) {
         CircleArea_Exam cp = new CircleArea_Exam();
         System.out.println("원의 면적은" + cp.area(10));
