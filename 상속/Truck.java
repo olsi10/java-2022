4b0823544c0c73478d257150ea1cc874d5637af5
olsi10 <wangbampang2035@gmail.com>
2022-04-28 00:32:57 +0900

    상위 클래스에 인자없는 생성자 하위 클래스에 super() 메소드 명시적 호출

--- 상속/Truck.java
+++ 상속/Truck.java
@@ -2,13 +2,16 @@ package 상속;
 //상속과 생성자
 
 class Car2 {
-    Car2() {
+    //Car2(String name) //error 인자없는 생성자 필요
+    //에러 수정 방법 디폴트 생성자 사용 () {} 하거나 하위 클래스에 super() 메소드로 명시적 호출
+    Car2() { // 상위 클래스에 들어간 인자없는 생성자
         System.out.println("Car 생성자");
     }
 }
 
 class Truck2 extends Car2 {
     Truck2() {
+    //super("SM3"); 하위 클래스에 super 메소드로 명시적 호출하는 방법    
         System.out.println("Truck 생성자");
     }
 
