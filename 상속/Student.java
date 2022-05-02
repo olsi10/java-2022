d5e62455afec2132956f66d66abe93e23d13dfad
olsi10 <wangbampang2035@gmail.com>
2022-05-02 16:08:57 +0900

    Student를 상속받는 Leader 클래스에 멤버 변수, 메소드 추가

--- 상속/Student.java
+++ 상속/Student.java
@@ -19,5 +19,13 @@ public class Student {
 //학생 클래스를 상속받는 학급회장 클래스
 class Leader extends Student {
     //멤버 변수나 메소드 정의 가능
+
+    //학급 회장인지 아닌지 구별하는 멤버 변수 추가
+    Boolean isLeader;
+
+    //인사하다 라는 메소드 추가
+    public void gretting() {
+        System.out.println("안녕!");
+    }
 }
 
