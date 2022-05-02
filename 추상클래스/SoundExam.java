9118b2a9fc29b0c3fec7ab7ba1501200612e2adc
olsi10 <wangbampang2035@gmail.com>
2022-05-02 23:30:40 +0900

    TV와 Radio 클래스가 Sound 인터페이스를 implements 했기 때문에 SoundUp() 메소드가 SoundDown() 메소드를 오버라이딩 함

---
+++ 추상클래스/SoundExam.java
@@ -0,0 +1,52 @@
+package 추상클래스;
+
+interface Sound {
+    public void SoundUp(int level);
+    public void SoundDown(int level);
+}
+
+class TV implements Sound {
+    private int SndLevel;
+
+    public TV() {
+        SndLevel = 0;
+    }
+
+    public void SoundUp(int level) {
+        SndLevel += level;
+    }
+
+    public void SoundDown(int level) {
+        SndLevel -= level;
+        if (SndLevel < 0) {
+            SndLevel = 0;
+        }
+    }
+}
+
+    class Radio implements Sound {
+        private int SndLevel;
+
+        public Radio() {
+            SndLevel = 0;
+        }
+
+        public void SoundUp(int level) {
+            SndLevel += level;
+        }
+
+        public void SoundDown(int level) {
+            SndLevel -= level;
+            if (SndLevel < 0) {
+                SndLevel = 0;
+            }
+        }
+}
+public class SoundExam {
+    public static void main(String[] args) {
+        Sound radio = new Radio();
+        Sound tv = new TV();
+        radio.SoundUp(5);
+        radio.SoundUp(5);
+    }
+}
