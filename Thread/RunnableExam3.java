ee72f282abb81e35d4a43f45fc0a2b68e20bc365
olsi10 <wangbampang2035@gmail.com>
2022-10-02 20:13:44 +0900

    Thread, Exception

---
+++ Thread/RunnableExam3.java
@@ -0,0 +1,25 @@
+package Thread;
+
+class Top2 implements Runnable{
+    public void run() {
+        for(int i=0; i<30; i++)
+            System.out.print((Thread.currentThread()).getName()+i+" \t");
+    }
+}
+
+public class RunnableExam3 {
+    public static void main(String[] args) {
+        System.out.println("프로그램 시작");
+        Top2 t = new Top2();
+        Thread thd1 = new Thread(t, "a"); // ㉡
+        Thread thd2 = new Thread(t, "b"); // ㉢
+        thd1.setPriority(9);
+        thd1.setName("New_A");
+        thd2.setName("New_B");
+        System.out.println(thd1.getPriority()); // ㉣
+        System.out.println(thd2.getPriority()); // ㉤
+        thd1.start();
+        thd2.start();
+        System.out.println("프로그램 종료");
+    }
+}
